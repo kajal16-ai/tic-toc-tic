22f400001b77cc99f7dfae19eb77cc76d433d69b
Kajal16-ai <kajal.hathaliya1999@gmail.com>
2025-06-23 16:21:19 +0530

    my first html file

--- tictocticgame/settings.py
+++ tictocticgame/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mygame/static')]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_URL = 'media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
